a1c00cc4b632d47546fc5a8924c937539a749c7f
bernicebetito <bernice_betito@dlsu.edu.ph>
2021-11-22 16:18:22 +0800

    combined gui and grid generation

--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
-import miner, gridSquare, random, math
+import miner, gridSquare, random, math, pygame, os
+
 
 # searching for gold for beacon placement
 def searchTop(grid, currentRow, currentCol):
@@ -10,14 +11,16 @@ def searchTop(grid, currentRow, currentCol):
 
     return False
 
+
 def searchBottom(grid, currentRow, currentCol, gridNumber):
     tempRow = currentRow + 1
     while tempRow < gridNumber:
         if grid[tempRow][currentCol] == "GOLD":
             return True
-        tempRow +=1
+        tempRow += 1
 
     return False
+
 
 def searchRight(grid, currentRow, currentCol, gridNumber):
     tempCol = currentCol + 1
@@ -25,8 +28,9 @@ def searchRight(grid, currentRow, currentCol, gridNumber):
         if grid[currentRow][tempCol] == "GOLD":
             return True
         tempCol += 1
-    
+
     return False
+
 
 def searchLeft(grid, currentRow, currentCol):
     tempCol = currentCol - 1
@@ -34,16 +38,17 @@ def searchLeft(grid, currentRow, currentCol):
         if grid[currentRow][tempCol] == "GOLD":
             return True
         tempCol -= 1
-    
+
     return False
 
-def generateGrid(gridNumber): # generate grid blueprint
+
+def generateGrid(gridNumber):  # generate grid blueprint
     pits = math.ceil(gridNumber * 0.25)
     beacons = math.ceil(gridNumber * 0.1)
 
     # samplingList = ["PIT", "GOLD", "EMPTY"]
-    counts = {"PIT" : 0, "BEACON" : 0, "GOLD" : 0, "EMPTY": 0}
-    maxcounts = {"PIT" : pits, "BEACON" : beacons, "GOLD" : 1, "EMPTY": 999999}
+    counts = {"PIT": 0, "BEACON": 0, "GOLD": 0, "EMPTY": 0}
+    maxcounts = {"PIT": pits, "BEACON": beacons, "GOLD": 1, "EMPTY": 999999}
 
     grid = []
 
@@ -52,7 +57,7 @@ def generateGrid(gridNumber): # generate grid blueprint
         column = []
 
         for columns in range(gridNumber):
-            if rows == 0 and columns == 0: # miner
+            if rows == 0 and columns == 0:  # miner
                 column.append("MINER")
                 continue
 
@@ -75,8 +80,8 @@ def generateGrid(gridNumber): # generate grid blueprint
         randCol = random.randint(0, gridNumber - 1)
 
         if grid[randRow][randCol] == "EMPTY":
-                grid[randRow][randCol] = "GOLD"
-                counts["GOLD"] += 1
+            grid[randRow][randCol] = "GOLD"
+            counts["GOLD"] += 1
 
     # place beacons
     while counts["BEACON"] < maxcounts["BEACON"]:
@@ -95,7 +100,7 @@ def generateGrid(gridNumber): # generate grid blueprint
         # grid square is in lower left
         if randRow == (gridNumber - 1) and randCol == 0 and grid[randRow][randCol] == "EMPTY":
             topResult = searchTop(grid, randRow, randCol)
-            rightResult = searchRight(grid, randRow, randCol, gridNumber) 
+            rightResult = searchRight(grid, randRow, randCol, gridNumber)
 
             if topResult == True or rightResult == True:
                 grid[randRow][randCol] = "BEACON"
@@ -113,7 +118,7 @@ def generateGrid(gridNumber): # generate grid blueprint
         # grid square is anywhere where empty
         elif grid[randRow][randCol] == "EMPTY":
             topResult = searchTop(grid, randRow, randCol)
-            rightResult = searchRight(grid, randRow, randCol, gridNumber) 
+            rightResult = searchRight(grid, randRow, randCol, gridNumber)
             bottomResult = searchBottom(grid, randRow, randCol, gridNumber)
             leftResult = searchLeft(grid, randRow, randCol)
 
@@ -123,36 +128,98 @@ def generateGrid(gridNumber): # generate grid blueprint
 
     return grid
 
-def generateGridSquares(grid): # returns a grid of gridSquare class elements using a grid blueprint
+
+def generateGridSquares(grid):  # returns a grid of gridSquare class elements using a grid blueprint
     trueGrid = []
     for rows in grid:
         column = []
         for columns in rows:
             if columns == "MINER":
                 gridElement = miner.Miner("RIGHT")
-                
+
             else:
                 gridElement = gridSquare.gridSquare(columns)
 
             column.append(gridElement)
 
         trueGrid.append(column)
-    
+
     return trueGrid
 
-grid = generateGrid(8)
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('MCO1: Miner')
+done = False
+
+font_main = pygame.font.SysFont(None, 50)
+text_main = font_main.render("MCO1: Miner", True, (75, 75, 226))
+font_sub = pygame.font.SysFont(None, 25)
+text_sub = font_sub.render("Group 42 - CSINTSY", True, (102, 102, 102))
+
+font_icons = pygame.font.SysFont(None, 35)
+pit_icon = font_icons.render("P", True, (120, 85, 137))
+beacon_icon = font_icons.render("B", True, (73, 109, 219))
+
+directory = os.getcwd()
+miner_icon = pygame.image.load(directory + r'\assets\miner_icon.png')
+miner_icon = pygame.transform.scale(miner_icon, (25, 25))
+
+gold_icon = pygame.image.load(directory + r'\assets\gold_icon.png')
+gold_icon = pygame.transform.scale(gold_icon, (25, 25))
+
+font_direction = pygame.font.SysFont(None, 30)
+curr_direction = font_direction.render("Current Direction: East", True, (240, 246, 246))
+
+box_width = 35
+box_height = 35
+box_margin = 7
+
+n = 8
+grid = generateGrid(n)
 for column in grid:
     print(column)
 
 trueGrid = generateGridSquares(grid)
-for row in trueGrid:
-    for column in row:
-        print(column.getContent(), end=" ")
-    print("\n")
 
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
 
+    screen.fill((25, 25, 25))
 
+    screen.blit(text_main, (320 - text_main.get_width() // 2, text_main.get_height() // 2))
+    screen.blit(text_sub, (320 - text_sub.get_width() // 2, text_main.get_height() + 10 + text_sub.get_height() // 2))
 
-            
+    row_ctr = 0
+    for row in trueGrid:
+        col_ctr = 0
+        for column in row:
+            if column.getContent() == "MINER" or column.getContent() == "GOLD" or column.getContent() == "PIT" or column.getContent() == "BEACON":
+                if column.getContent() == "MINER":
+                    print_icon = miner_icon
+                elif column.getContent() == "GOLD":
+                    print_icon = gold_icon
+                elif column.getContent() == "PIT":
+                    print_icon = pit_icon
+                elif column.getContent() == "BEACON":
+                    print_icon = beacon_icon
 
-            
+                screen.blit(print_icon,
+                            [((box_margin + box_width) * col_ctr + box_margin) + (320 - ((box_margin + box_width) * 8) // 2) + 8,
+                             ((box_margin + box_height) * row_ctr + box_margin) + text_main.get_height() + 10 + text_sub.get_height() + 20 + 8,
+                             box_width,
+                             box_height])
+            else:
+                pygame.draw.rect(screen, (200, 200, 200),
+                                 [((box_margin + box_width) * col_ctr + box_margin) + (320 - ((box_margin + box_width) * 8) // 2),
+                                  ((box_margin + box_height) * row_ctr + box_margin) + text_main.get_height() + 10 + text_sub.get_height() + 20,
+                                  box_width,
+                                  box_height])
+            col_ctr += 1
+        row_ctr += 1
+
+    direction_height = ((box_margin + box_height) * n + box_margin) + text_main.get_height() + 10 + text_sub.get_height() + 30
+    screen.blit(curr_direction, (320 - curr_direction.get_width() // 2, direction_height))
+    pygame.display.flip()
